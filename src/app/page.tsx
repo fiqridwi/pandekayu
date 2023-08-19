d51340dc42b287cb0a9538d06ed2436bcb52beb8
Fiqri Dwi <nurfiqri23@gmail.com>
2023-08-19 17:15:20 +0700

    refactor: remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
 import Hero from "./components/home/Hero";
-import Navbar from "./components/home/Navbar";
 import InfoSection from "./components/home/InfoSection";
 import PandeProjectSection from "./components/home/PandeProjectSection";
-import Footer from "./components/home/Footer";
-import RootLayout from "./layout";
 
-// import { main.scss } from 'public/styles';
 export default function Home() {
 	return (
 		<main>
-			{/* <Navbar /> */}
 			<Hero />
 			<InfoSection />
 			<PandeProjectSection />
-			{/* <Footer /> */}
 		</main>
 	);
 }
